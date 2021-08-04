cf38c46ae184e33fd493080f819f7ec6bb89e0dc
Yuri_1967 <9022368@mail.ru>
2021-08-04 15:58:25 +0500

    Задача 2

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,24 @@
 на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
 '''
 
+from random import randint
+
+
+def sort(array):
+    res = []
+    new_arr = arr[:]
+    for _ in range(len(new_arr)):
+        mini = min(new_arr)
+        mini_index = new_arr.index(mini)
+        res.append(mini)
+        del new_arr[mini_index]
+    return res
+
+
+N = 20
+arr = []
+for i in range(N):
+    arr.append(randint(0, 50))
+
+print(arr)
+print(sort(arr))
