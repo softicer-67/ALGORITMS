a688122cfaffdbd69cd42dd52a1080664caa0a68
Yuri_1967 <9022368@mail.ru>
2021-08-04 15:50:28 +0500

    Задача 1

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,24 @@
 Выведите на экран исходный и отсортированный массивы. Сортировка должна быть реализована в виде функции.
 По возможности доработайте алгоритм (сделайте его умнее).
 '''
+
+from random import randint
+
+
+def bubble(array):
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if array[j] > array[j + 1]:
+                buff = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = buff
+
+
+N = 20
+arr = []
+for i in range(N):
+    arr.append(randint(-100, 100))
+
+print(arr)
+bubble(arr)
+print(arr)
