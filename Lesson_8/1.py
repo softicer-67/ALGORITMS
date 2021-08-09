653cb9e1ad5629eb43128b2955bbcf3993132bd0
Yuri_1967 <9022368@mail.ru>
2021-08-10 02:27:55 +0500

    Задача 1

--- Lesson_8/1.py
+++ Lesson_8/1.py
@@ -11,8 +11,8 @@ s = 'hello world'
 sum_substring = set()
 
 for i in range(len(s)):
-    for j in range(len(s), i, -1):
+    for j in range(len(s), i, - 1):
         hash_str = hashlib.sha1(s[i:j].encode('utf-8')).hexdigest()
         sum_substring.add(hash_str)
 
-print(f'{len(sum_substring) -1} различных подстрок в строке {s}')
+print(len(sum_substring) - 1, 'различных подстрок в строке', s)
